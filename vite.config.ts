b2f74e7e61effd3ca9d80e97b23e56f8bb030cd0
unknown <guy.iserlis.dev@gmail.com>
2023-05-07 19:19:20 +0300

    change build target

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../final-project-ts-be-prisma-atlas/src/public",
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-      },
-    },
-  },
 });
