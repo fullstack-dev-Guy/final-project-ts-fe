8b76d6dc6455a35411e67eb7c7b1658a9ac531d3
unknown <guy.iserlis.dev@gmail.com>
2023-05-20 10:40:24 +0300

    fixing the blog html dir=rtl lang=he

--- src/components/MyAllBlogs/MyAllBlogs.tsx
+++ src/components/MyAllBlogs/MyAllBlogs.tsx
@@ -16,77 +16,77 @@ export default function MyAllBlogs() {
 
   if (blogs.status === "error") return <p>{blogs.message}</p>;
   return (
-    <div className="mx-auto mt-44 max-w-screen-xl p-6">
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6">
-          <div className="mx-auto mb-6 max-w-screen-sm text-center lg:mb-6">
-            <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white lg:text-4xl">
-              הבלוגים שלי
-            </h2>
-            <p className="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
-              .כאן אפשר לרשום את הבלוג או את חוויית הביקור שהיתה לכם אצלנו
-            </p>
-            <p className="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
-              .ניתן למחוק , להוסיף בלוג או לערוך בלוגים קיימים
-            </p>
-          </div>
+    <html dir="rtl" lang="he">
+      <head>
+        <meta charSet="UTF-8" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre&display=swap"
+        />
+      </head>
+      <div className="mx-auto mt-44 max-w-screen-xl p-6">
+        <section className="bg-white dark:bg-gray-900">
+          <div className="mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6">
+            <div className="mx-auto mb-6 max-w-screen-sm text-center lg:mb-6">
+              <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white lg:text-4xl">
+                הבלוגים שלי
+              </h2>
+              <p className="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
+                .כאן אפשר לרשום את הבלוג או את חוויית הביקור שהיתה לכם אצלנו
+              </p>
+              <p className="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
+                .ניתן למחוק , להוסיף בלוג או לערוך בלוגים קיימים
+              </p>
+            </div>
 
-          <div className="  mb-2 flex justify-center">
-            {user ? (
-              <button
-                onClick={() => navigate("/blogs/addblog")}
-                className=" rounded-lg border border-gray-700 bg-yellow-200 p-2 text-base  font-semibold  text-violet-700 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-violet-300 active:bg-yellow-500 dark:border-gray-600  dark:bg-gray-800 dark:text-white dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-              >
-                צור בלוג חדש
-              </button>
-            ) : null}
-          </div>
-
-          <div className="flex-auto">
-            {myBlogsArry ? (
-              myBlogsArry.map((blog) => (
-                <article
-                  key={blog.id}
-                  className="mb-4 rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800"
+            <div className="  mb-2 flex justify-center">
+              {user ? (
+                <button
+                  onClick={() => navigate("/blogs/addblog")}
+                  className=" rounded-lg border border-gray-700 bg-yellow-200 p-2 text-base  font-semibold  text-violet-700 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-violet-300 active:bg-yellow-500 dark:border-gray-600  dark:bg-gray-800 dark:text-white dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                 >
-                  <p>{blog.id}</p>
-                  <div className=" ">
-                    <h2 className=" mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
-                      {blog.title}
-                    </h2>
-                    <p className="mb-5 text-center font-light text-gray-500 dark:text-gray-400 ">
-                      {blog.body}
-                    </p>
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center space-x-4">
-                      <span className="font-medium dark:text-white">
-                        {blog.date}
-                      </span>
-                    </div>
+                  צור בלוג חדש
+                </button>
+              ) : null}
+            </div>
 
-                    <div className="mt-2 text-center">
-                      <button
-                        className="border p-2 "
-                        onClick={() => navigate("/blogs/blogpage/" + blog.id)}
-                      >
-                        ... קרא עוד
-                      </button>
-                      <div className="mt-2">
+            <div className="flex-auto">
+              {myBlogsArry ? (
+                myBlogsArry.map((blog) => (
+                  <article
+                    key={blog.id}
+                    className="mb-4 rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800"
+                  >
+                    <p>{blog.id}</p>
+                    <div className=" ">
+                      <h2 className=" mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
+                        {blog.title}
+                      </h2>
+                      <p className="mb-5 text-center font-light text-gray-500 dark:text-gray-400 ">
+                        {blog.body}
+                      </p>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <div className=" mt-2 flex flex-wrap text-center">
+                        <button
+                          className="mx-3 mt-2 border p-2"
+                          onClick={() => navigate("/blogs/blogpage/" + blog.id)}
+                        >
+                          קרא עוד
+                        </button>
+
                         {user ? (
                           <button
-                            className="border p-2 "
+                            className="mx-3 mt-2 border p-2"
                             onClick={() => navigate("deleteblog/" + blog.id)}
                           >
                             מחיקת בלוג
                           </button>
                         ) : null}
-                      </div>
 
-                      <div className="mt-2">
                         {user ? (
                           <button
-                            className="border p-2 "
+                            className="mx-3 mt-2 border p-2"
                             onClick={() =>
                               navigate("/blogs/editblog/" + blog.id)
                             }
@@ -95,20 +95,25 @@ export default function MyAllBlogs() {
                           </button>
                         ) : null}
                       </div>
+                      <div className="flex items-center space-x-4">
+                        <span className="font-medium dark:text-white">
+                          {blog.date}
+                        </span>
+                      </div>
                     </div>
-                  </div>
-                </article>
-              ))
-            ) : (
-              <p>Loading...</p>
-            )}
+                  </article>
+                ))
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
+            <div className="flex justify-center">
+              <Outlet />
+            </div>
           </div>
-          <div className="flex justify-center">
-            <Outlet />
-          </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </html>
   );
 }
 
--- src/routes/Blogs/Blogs.tsx
+++ src/routes/Blogs/Blogs.tsx
@@ -11,71 +11,80 @@ export default function Blogs() {
 
   // if (blogs.status === "error") return <p>{blogs.message}</p>;
   return (
-    <div className="mx-auto mt-44 max-w-screen-xl p-6">
-      <section className="bg-white dark:bg-gray-900">
-        <div className="mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6">
-          <div className="mx-auto mb-6 max-w-screen-sm text-center lg:mb-6">
-            <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white lg:text-4xl">
-              הבלוג שלנו
-            </h2>
-            <p className="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
-              כאן תוכלו לרשום את הבלוג שלכם או את חוויית הביקור שהיתה לכם אצלנו
-              , על מנת לרשום תוכן יש צורך להרשם ולהתחבר למערכת. לאחר מכן תוכלו
-              לערוך או למחוק את התוכן הכתוב
-            </p>
-          </div>
+    <html dir="rtl" lang="he">
+      <head>
+        <meta charSet="UTF-8" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Frank+Ruhl+Libre&display=swap"
+        />
+      </head>
+      <div className="mx-auto mt-44 max-w-screen-xl p-6">
+        <section className="bg-white dark:bg-gray-900">
+          <div className="mx-auto max-w-screen-xl py-8 px-4 lg:py-16 lg:px-6">
+            <div className="mx-auto mb-6 max-w-screen-sm text-center lg:mb-6">
+              <h2 className="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white lg:text-4xl">
+                הבלוג שלנו
+              </h2>
+              <p className="font-light text-gray-500 dark:text-gray-400 sm:text-xl">
+                כאן תוכלו לרשום את הבלוג שלכם או את חוויית הביקור שהיתה לכם
+                אצלנו , על מנת לרשום תוכן יש צורך להרשם ולהתחבר למערכת. לאחר מכן
+                תוכלו לערוך או למחוק את התוכן הכתוב
+              </p>
+            </div>
 
-          <div className="  mb-2 flex justify-center">
-            {user ? (
-              <button
-                onClick={() => navigate("addblog")}
-                className=" rounded-lg border border-gray-700 bg-yellow-200 p-2 text-base  font-semibold  text-violet-700 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-violet-300 active:bg-yellow-500 dark:border-gray-600  dark:bg-gray-800 dark:text-white dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-              >
-                צור בלוג חדש
-              </button>
-            ) : null}
-          </div>
-
-          <div className="flex-auto">
-            {blogs.data ? (
-              blogs.data.map((blog) => (
-                <article
-                  key={blog.id}
-                  className="mb-4 rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800"
+            <div className="  mb-2 flex justify-center">
+              {user ? (
+                <button
+                  onClick={() => navigate("addblog")}
+                  className=" rounded-lg border border-gray-700 bg-yellow-200 p-2 text-base  font-semibold  text-violet-700 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-violet-300 active:bg-yellow-500 dark:border-gray-600  dark:bg-gray-800 dark:text-white dark:text-white dark:hover:border-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
                 >
-                  <div className=" ">
-                    <h2 className=" mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
-                      {blog.title}
-                    </h2>
-                    <p className="mb-5 text-center font-light text-gray-500 dark:text-gray-400 ">
-                      {blog.body}
-                    </p>
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center space-x-4">
-                      <span className="font-medium dark:text-white">
-                        {blog.date}
-                      </span>
-                    </div>
+                  צור בלוג חדש
+                </button>
+              ) : null}
+            </div>
 
-                    <div className="mt-2">
-                      <button onClick={() => navigate("blogpage/" + blog.id)}>
-                        ... קרא עוד
-                      </button>
+            <div className="flex-auto">
+              {blogs.data ? (
+                blogs.data.map((blog) => (
+                  <article
+                    key={blog.id}
+                    className="mb-4 rounded-lg border border-gray-200 bg-white p-6 shadow-md dark:border-gray-700 dark:bg-gray-800"
+                  >
+                    <div className=" ">
+                      <h2 className=" mb-2 text-center text-2xl font-bold tracking-tight text-gray-900 dark:text-white ">
+                        {blog.title}
+                      </h2>
+                      <p className="mb-5 text-center font-light text-gray-500 dark:text-gray-400 ">
+                        {blog.body}
+                      </p>
                     </div>
-                  </div>
-                </article>
-              ))
-            ) : (
-              <p>Loading...</p>
-            )}
+                    <div className="flex items-center justify-between">
+                      <div className="flex items-center space-x-4">
+                        <span className="font-medium dark:text-white">
+                          {blog.date}
+                        </span>
+                      </div>
+
+                      <div className="mt-2">
+                        <button onClick={() => navigate("blogpage/" + blog.id)}>
+                          קרא עוד
+                        </button>
+                      </div>
+                    </div>
+                  </article>
+                ))
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
+            <div className="flex justify-center">
+              <Outlet />
+            </div>
           </div>
-          <div className="flex justify-center">
-            <Outlet />
-          </div>
-        </div>
-      </section>
-    </div>
+        </section>
+      </div>
+    </html>
   );
 }
 
